7851b98060441ff23c8e89540475e714b6751a9f
LeoTerryMaster <leandrogomes.santos.terrymaster@gmail.com>
2025-12-03 20:35:36 -0300

    configuracoes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-
+  base: '/pokemon/', // nome do repositório no GitHub
   plugins: [
     react(),
     tailwindcss(),
